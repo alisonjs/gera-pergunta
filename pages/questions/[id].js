17aaad4801bce143dc13f319316c507ee149c972
Alison Silva <alison.dev.silva@gmail.com>
2021-02-19 22:04:59 -0300

    Add answer page

--- pages/questions/[id].js
+++ pages/questions/[id].js
@@ -1,11 +1,45 @@
 import Head from 'next/head';
+import Navbar from 'components/navbar';
 
-function Questions(){
-  return (<div> 
-    <Head>
+function Questions() {
+  return (
+    <div>
+      <Head>
         <title>Gera Pergunta</title>
       </Head>
-    Cadastro de respostas</div>)
+
+      <div class="container">
+        <h1 class="mt-4">Pergunta</h1>
+        <p class="mt-4">Descrição</p>
+        <hr class="my-4" />
+        <form action="/answer" method="POST">
+          <div class="form-group">
+            <label>Resposta:</label>
+            <textarea
+              placeholder="Insira sua resposta aqui"
+              name="answer"
+              class="form-control"
+            ></textarea>
+          </div>
+          <input type="hidden" name="question" value="<%= question.id %>" />
+          <button type="submit" class="btn btn-primary">
+            Responder
+          </button>
+        </form>
+        <hr class="my-4" />
+        <h3>Respostas</h3>
+        <hr class="my-4" />
+        <div class="card my-4">
+          <div class="card-body">
+            <p class="card-text">Resposta</p>
+            <p class="card-text">
+              <small class="text-muted">Há 4 horas </small>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Questions
+export default Questions;
