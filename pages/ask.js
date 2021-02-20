b8097401d6f5f4d8ff3ab793e306ea8042b048ae
Alison Silva <alison.dev.silva@gmail.com>
2021-02-19 22:01:54 -0300

    Add ask page

--- pages/ask.js
+++ pages/ask.js
@@ -1,11 +1,46 @@
 import Head from 'next/head';
+import Navbar from 'components/navbar';
 
-function Ask(){
-  return (<div> 
-    <Head>
+function Ask() {
+  return (
+    <div>
+      <Head>
         <title>Gera Pergunta</title>
       </Head>
-    Cadastro de pergunta</div>)
+      <Navbar />
+      <div class="container">
+        <div class="card mt-3">
+          <div class="card-header">
+            <h3>Realizar pergunta</h3>
+          </div>
+          <div class="card-body">
+            <form id="ask-form" method="POST" action="/questions">
+              <div class="form-group">
+                <label>Título</label>
+                <input
+                  type="text"
+                  placeholder="Título"
+                  name="title"
+                  class="form-control my-2"
+                />
+              </div>
+              <div class="form-group">
+                <label>Descrição</label>
+                <textarea
+                  placeholder="Descreva sua pergunta aqui"
+                  name="description"
+                  class="form-control my-2"
+                ></textarea>
+              </div>
+              <button type="submit" class="btn btn-primary">
+                Perguntar
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Ask
+export default Ask;
